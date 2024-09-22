845a9856135359c43b3608716d3d254a847f0bed
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-09-22 00:05:53 -0300

    feat: create a dedicated file for the routes and a unique verification

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,52 +1,25 @@
 // CommonJS ==> Padrão de importação utilizando o require
 // ESModules ==> Padrão de importação utilizando o import/export
 
-import http from 'node:http'
-import { randomUUID } from 'node:crypto'
-import { Database } from './database.js'
-import { json } from './middlewares/json.js'
-
-
-
-const database = new Database()
-
+import http from "node:http";
+import { json } from "./middlewares/json.js";
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
-    const { method, url } = req
+  const { method, url } = req;
 
-    await json(req, res)
-    
-    // Rotas HTTP são portas de entradas para executar diferente ações na aplicação
-    if (method == 'GET' && url == '/users') {
-        const users = database.select('users')
+  await json(req, res);
 
-        return res.end(JSON.stringify(users))
-        .setHeader('Content-type', 'application/json')
-    }
+  const route = routes.find(route => {
+    return route.method === method && route.path === url;
+  })
 
-    if (method == 'POST' && url == '/users') {
-        const { name, email } = req.body
-
-        const user = {
-            id: randomUUID(),
-            name, 
-            email, 
-        }
-
-        database.insert('users', user)
-
-        // HTTP status code que simboliza uma resposta de sucesso
-        return res.writeHead(201).end()
-    }
-
-    // HTTP status code que simboliza que a rota não existe
-    return res.writeHead(404).end('Not found')
-
-})
+  if (route) {
+    return route.handler(req, res)
+  }
 
 
-server.listen(3333)
+  return res.writeHead(404).end("Not found");
+});
 
-
-
-
+server.listen(3333);
